53e111715eb9c322ac6c1b462b252b14eb3f3208
donaam <112537799+donaam@users.noreply.github.com>
2022-08-31 13:18:38 +0530

    Add files via upload

---
+++ Operators3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 7;
+        if (x != y) {
+            System.out.println("x and y are not equal.");
+        } else {
+            System.out.println("x and y are equal.");
+        }
+    }
+}
